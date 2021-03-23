6d2af62eca3173ebf6ecaa2a3bfdc311498d309c
Kaspar Schleiser <kaspar@schleiser.de>
2021-03-23 13:44:00 +0100

    src/tests: 16_tasks: test some vars

--- src/tests/16_tasks/test.sh
+++ src/tests/16_tasks/test.sh
@@ -6,6 +6,8 @@ cleanup
 
 ${LAZE} task echo foo | grep -s "^foo$"
 ${LAZE} task foobar | grep -s "^foobar$"
+${LAZE} task --global --app subdir --builder default vars \
+         |grep -s "^relpath=subdir relroot=.. out=build/default/subdir/subdir.elf builder=default$"
 
 echo TEST_OK
 
