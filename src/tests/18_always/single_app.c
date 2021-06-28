f0ad009cadac40b92e3f95f0f274d14c1a304ef2
Kaspar Schleiser <kaspar@schleiser.de>
2021-06-28 15:00:44 +0200

    tests: update for "always" support

---
+++ src/tests/18_always/single_app.c
@@ -0,0 +1,1 @@
+content of single_builder.c
---
+++ src/tests/18_always/test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+. ../test-common.sh
+
+cleanup
+echo "building first time"
+build
+grep -vq "overwrite" build/single_builder/single_app/single_app.elf || exit 1
+echo "building second time"
+build
+clean_temp_files
+
+diff -r build build_expected
+
+echo TEST_OK
+
+cleanup
