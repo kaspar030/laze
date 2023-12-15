f544669a588652be8723463c3ded2498f9f25a70
Kaspar Schleiser <kaspar@schleiser.de>
2023-12-15 14:05:18 +0100

    tests: add support for testing stdout/stderr and patters in those

--- src/tests/test-common.sh
+++ src/tests/test-common.sh
@@ -9,12 +9,32 @@ build() {
         set +e
     fi
 
-    ${LAZE} build -g "$@"
+    ${LAZE} build -g "$@" > stdout 2> stderr
     EXIT_CODE=$?
 
     if [ -f EXPECTED_EXIT_CODE ]; then
         set -e
         test "$EXIT_CODE" = "$(cat EXPECTED_EXIT_CODE)"
+    fi
+
+    if [ -f EXPECTED_STDOUT ]; then
+        echo testing stdout
+        diff -q EXPECTED_STDOUT stdout
+    fi
+
+    if [ -f EXPECTED_STDERR ]; then
+        echo testing stderr
+        diff -q EXPECTED_STDERR stderr
+    fi
+
+    if [ -f EXPECTED_STDOUT_PATTERNS ]; then
+        echo testing stdout patterns
+        grep --silent -f EXPECTED_STDOUT_PATTERNS stdout
+    fi
+
+    if [ -f EXPECTED_STDERR_PATTERNS ]; then
+        echo testing stderr patterns
+        grep --silent -f EXPECTED_STDERR_PATTERNS stderr
     fi
 }
 
@@ -23,7 +43,8 @@ clean_temp_files() {
         build/.ninja_log build/.ninja_deps \
         build/laze-cache-local.bincode \
         build/laze-cache-global.bincode \
-        compile_commands.json
+        compile_commands.json \
+        stdout stderr
 }
 
 : "${LAZE:=laze}"
