85768f33568d84b5cc5976bc0afea6b9799f9ec1
Kaspar Schleiser <kaspar@schleiser.de>
2023-12-22 13:36:42 +0100

    utils: introduce `ContainingPath` trait

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,9 @@
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
 
+use camino::Utf8PathBuf;
+use indexmap::IndexMap;
+
 pub(crate) fn calculate_hash<T: Hash>(t: &T) -> u64 {
     let mut s = DefaultHasher::new();
     t.hash(&mut s);
@@ -19,4 +22,18 @@ pub enum StringOrMapVecString {
 pub enum StringOrMapString {
     String(String),
     Map(std::collections::HashMap<String, String>),
+}
+
+pub(crate) trait ContainingPath<T: AsRef<std::path::Path>> {
+    fn get_containing_path(&self, path: &T) -> Option<&T>;
+}
+
+impl ContainingPath<Utf8PathBuf> for IndexMap<&Utf8PathBuf, Utf8PathBuf> {
+    fn get_containing_path(&self, path: &Utf8PathBuf) -> Option<&Utf8PathBuf> {
+        self.get(path).or_else(|| {
+            self.iter()
+                .find(|(k, _)| path.starts_with(k))
+                .map(|(_, v)| v)
+        })
+    }
 }
