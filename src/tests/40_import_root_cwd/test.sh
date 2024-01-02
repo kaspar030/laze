a25bd6d30f4f092adafaf113d5fbf7df96673e13
Kaspar Schleiser <kaspar@schleiser.de>
2024-01-02 21:49:36 +0100

    tests: add 40_import_root_cwd

---
+++ src/tests/40_import_root_cwd/test.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+rm -Rf testrepo
+git init testrepo
+cp imported.yml testrepo/laze.yml
+git -C testrepo add .
+git -C testrepo commit -m ...
+
+. ../test-common.sh
+
+cleanup
+
+${LAZE} build -g
+
+clean_temp_files
+rm -Rf testrepo
+rm -Rf build/imports
+
+diff -r build build_expected
+
+echo TEST_OK
+
+cleanup
