311ee37895949d46f6a77b0e33687d5c99ef050b
Deploy from CI <>
2026-03-13 15:58:52 +0000

    Deploy 90282dbf4a994cea4cee24e2b6e3a23a834ac1d8 to gh-pages

--- dev/searcher-c2a407aa.js
+++ dev/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-bdd2e215.js',
+                path_to_root + 'searchindex-fcaa0d17.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
